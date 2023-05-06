ac5bc8054a56ee0044f727ea7b03d4d722f0878c
Agor David <davidagort@gmail.com>
2023-05-06 05:39:33 +0100

    ext

--- reference/path_demo.js
+++ reference/path_demo.js
@@ -3,3 +3,9 @@ const path = require('path');
 
 // Base file name
 console.log(path.basename(__filename));
+
+// Directory name
+console.log(path.dirname(__dirname));
+
+//File Extenson
+console.log(path.extname(__filename));

e28d3dffad258e6d33b8986828c1d87bb86318c7
Agor David <davidagort@gmail.com>
2023-05-06 05:42:54 +0100

    concatenate paths

--- reference/path_demo.js
+++ reference/path_demo.js
@@ -12,3 +12,6 @@ console.log(path.extname(__filename));
 
 // Create path object
 console.log(path.parse(__filename));
+
+// Concatenate paths
+console.log(path.join(__dirname, 'test', 'hello.html'));

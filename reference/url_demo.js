a36b910b5ed6a6e6048d7e3057599a9f594e65ab
Agor David <davidagort@gmail.com>
2023-05-09 03:09:55 +0100

    get host name

--- reference/url_demo.js
+++ reference/url_demo.js
@@ -4,3 +4,8 @@ const myUrl = new URL('https://mywebsite.com/hello.html?id=100&status=active');
 
 // Serialized URL
 console.log(myUrl.href);
+
+// Host (root domaain)
+console.log("Host name: "+ myUrl.host);
+
+// 

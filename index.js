7af84addc032cc3184ef190144701405e5750c72
Agor David <davidagort@gmail.com>
2023-05-09 04:49:40 +0100

    load about page to server

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ const server = http.createServer((req, res) => {
         );
     };
 
+    if (req.url == '/about') {
+        fs.readFile(path.join(__dirname, 'public', 'about.html'),
+        (err, content) => {
+            if (err) throw err;
+            res.writeHead(200, {'content-type': 'text/html'});
+            res.end(content);
+        })
+    }
+
 });
 
 const PORT = process.env.PORT || 5000
